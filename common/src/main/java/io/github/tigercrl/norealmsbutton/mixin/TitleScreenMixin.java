04c65c230e345cf64c1c49934b76219b2b5b5ef7
Tigercrl <tigercrl@icloud.com>
2023-09-22 18:04:05 +0800

    v1.0.1

--- common/src/main/java/io/github/tigercrl/norealmsbutton/mixin/TitleScreenMixin.java
+++ common/src/main/java/io/github/tigercrl/norealmsbutton/mixin/TitleScreenMixin.java
@@ -2,12 +2,9 @@ package io.github.tigercrl.norealmsbutton.mixin;
 
 import com.mojang.realmsclient.gui.screens.RealmsNotificationsScreen;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -15,21 +12,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin {
-    @Shadow
-    @Nullable
-    private RealmsNotificationsScreen realmsNotificationsScreen;
-
     @Inject(method = "realmsNotificationsEnabled", at = @At("RETURN"), cancellable = true)
     public void realmsNotificationsEnabled(CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(false);
     }
 
-    @Redirect(method = "createNormalMenuOptions", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/TitleScreen;addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;", ordinal = 2))
-    public GuiEventListener returnEmptyButton(TitleScreen instance, GuiEventListener guiEventListener) {
-        return Button.builder(Component.empty(), (button) -> {
-        }).build();
+    @Redirect(method = "createNormalMenuOptions", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/Button$Builder;bounds(IIII)Lnet/minecraft/client/gui/components/Button$Builder;"))
+    public Button.Builder returnEmptyRealmsButton(Button.Builder instance, int i, int j, int k, int l) {
+        if (instance.build().getMessage().equals(Component.translatable("menu.online")))
+            return instance.bounds(i, j - 24, 2, 0);
+        else return instance.bounds(i, j, k, l);
     }
 
     @Redirect(method = "added", at = @At(value = "INVOKE", target = "Lcom/mojang/realmsclient/gui/screens/RealmsNotificationsScreen;added()V"))
-    public void doNothing(RealmsNotificationsScreen instance) {}
+    public void doNothing(RealmsNotificationsScreen instance) {
+    }
 }
