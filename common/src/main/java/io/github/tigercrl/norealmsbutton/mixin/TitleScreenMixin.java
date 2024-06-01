85166a9c698bdc3d46aa25a5630aa56f715779c3
Tigercrl <tigercrl@icloud.com>
2024-06-01 21:14:09 +0800

    v1.0.2

--- common/src/main/java/io/github/tigercrl/norealmsbutton/mixin/TitleScreenMixin.java
+++ common/src/main/java/io/github/tigercrl/norealmsbutton/mixin/TitleScreenMixin.java
@@ -1,7 +1,10 @@
 package io.github.tigercrl.norealmsbutton.mixin;
 
 import com.mojang.realmsclient.gui.screens.RealmsNotificationsScreen;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.PlainTextButton;
+import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,19 +15,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin {
+
     @Inject(method = "realmsNotificationsEnabled", at = @At("RETURN"), cancellable = true)
     public void realmsNotificationsEnabled(CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(false);
-    }
-
-    @Redirect(method = "createNormalMenuOptions", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/Button$Builder;bounds(IIII)Lnet/minecraft/client/gui/components/Button$Builder;"))
-    public Button.Builder returnEmptyRealmsButton(Button.Builder instance, int i, int j, int k, int l) {
-        if (instance.build().getMessage().equals(Component.translatable("menu.online")))
-            return instance.bounds(i, j - 24, 2, 0);
-        else return instance.bounds(i, j, k, l);
+        Minecraft.getInstance().setConnectedToRealms(false);
     }
 
     @Redirect(method = "added", at = @At(value = "INVOKE", target = "Lcom/mojang/realmsclient/gui/screens/RealmsNotificationsScreen;added()V"))
     public void doNothing(RealmsNotificationsScreen instance) {
+        Minecraft.getInstance().setConnectedToRealms(false);
     }
 }
--- forge/src/main/java/io/github/tigercrl/norealmsbutton/forge/NoRealmsButtonForge.java
+++ forge/src/main/java/io/github/tigercrl/norealmsbutton/forge/NoRealmsButtonForge.java
@@ -2,7 +2,6 @@ package io.github.tigercrl.norealmsbutton.forge;
 
 import io.github.tigercrl.norealmsbutton.NoRealmsButton;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(NoRealmsButton.MOD_ID)
 public class NoRealmsButtonForge {
