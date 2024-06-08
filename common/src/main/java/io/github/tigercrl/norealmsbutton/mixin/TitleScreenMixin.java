6cc0bb641168b6dc02ad46a9011d22d113ecc4b2
Tigercrl <tigercrl@icloud.com>
2024-06-08 16:51:04 +0800

    v1.0.3

--- common/src/main/java/io/github/tigercrl/norealmsbutton/mixin/TitleScreenMixin.java
+++ common/src/main/java/io/github/tigercrl/norealmsbutton/mixin/TitleScreenMixin.java
@@ -1,16 +1,9 @@
 package io.github.tigercrl.norealmsbutton.mixin;
 
-import com.mojang.realmsclient.gui.screens.RealmsNotificationsScreen;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.PlainTextButton;
-import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.TitleScreen;
-import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TitleScreen.class)
@@ -19,11 +12,5 @@ public abstract class TitleScreenMixin {
     @Inject(method = "realmsNotificationsEnabled", at = @At("RETURN"), cancellable = true)
     public void realmsNotificationsEnabled(CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(false);
-        Minecraft.getInstance().setConnectedToRealms(false);
-    }
-
-    @Redirect(method = "added", at = @At(value = "INVOKE", target = "Lcom/mojang/realmsclient/gui/screens/RealmsNotificationsScreen;added()V"))
-    public void doNothing(RealmsNotificationsScreen instance) {
-        Minecraft.getInstance().setConnectedToRealms(false);
     }
 }
