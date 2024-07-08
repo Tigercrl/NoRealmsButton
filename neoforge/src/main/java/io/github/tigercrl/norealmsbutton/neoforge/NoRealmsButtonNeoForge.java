3e86c665204d2b9283284c7a725e2cf5246c4fcd
Tigercrl <tigercrl@icloud.com>
2024-07-08 20:13:15 +0800

    v1.1.0

---
+++ neoforge/src/main/java/io/github/tigercrl/norealmsbutton/neoforge/NoRealmsButtonNeoForge.java
@@ -0,0 +1,11 @@
+package io.github.tigercrl.norealmsbutton.neoforge;
+
+import io.github.tigercrl.norealmsbutton.NoRealmsButton;
+import net.neoforged.fml.common.Mod;
+
+@Mod(NoRealmsButton.MOD_ID)
+public class NoRealmsButtonNeoForge {
+    public NoRealmsButtonNeoForge() {
+        NoRealmsButton.init();
+    }
+}
